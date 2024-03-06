7af4b169e18045848c5db182626eaeebd21e8f2e
ineedjobtostartwork <ineedjobtostartwork@gmail.com>
2024-03-06 18:54:53 +0100

    ⚙️  Chore(tsup): change dev build process (add template folder to copy)

--- config/tsuprc/tsup.base.ts
+++ config/tsuprc/tsup.base.ts
@@ -1,8 +1,10 @@
+import { copy } from "esbuild-plugin-copy";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
 	entry: ["src/index.ts"],
 
 	clean: true,
-	format: ["cjs", "esm"]
+	format: ["cjs", "esm"],
+	esbuildPlugins: [copy({ assets: [{ from: "./src/templates/configs/*", to: "./templates/configs" }] })]
 });
